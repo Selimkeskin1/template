e8e0d38b6f39c0213de3ecc0d9f5acb3e033e384
Selimkeskin1 <selimkeskin1@gmail.com>
2026-04-14 23:28:24 +0300

    135

--- app/src/main/java/com/template/LoginReposity.kt
+++ app/src/main/java/com/template/LoginReposity.kt
@@ -1,16 +1,6 @@
 package com.template
-sealed class Result<out R> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
+
 
 
 class LoginRepository() {
-    private  val loginUrl = "https://example.com/login"
-
-    // Function that makes the network request, blocking the current thread
-    fun makeLoginRequest(
-        jsonBody: String){
-
-    }
 }
--- app/src/main/java/com/template/Model.kt
+++ app/src/main/java/com/template/Model.kt
@@ -9,6 +9,7 @@ class MyModel private constructor ( context  : Context) : DefaultLifecycleObserv
 
     private val appContext = context.applicationContext
 
+
     init {
         // Bu log sadece nesne İLK KEZ yaratıldığında basılır
         Log.d("MyModel", "NESNE YARATILDI! Adres: ${this.hashCode()}")
